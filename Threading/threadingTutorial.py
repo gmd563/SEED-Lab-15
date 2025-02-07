ddbc86d88b51cb6b66ed402783d971d9599b5a96
sophiamimlitz <sophiamimlitz@mines.edu>
2025-02-07 14:02:30 -0700

    Create threadingTutorial.py

---
+++ Threading/threadingTutorial.py
@@ -0,0 +1,46 @@
+import queue
+import threading
+from random import random
+from time import sleep
+
+import board
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+
+q = queue.Queue()
+
+
+def myFunction():
+	##########################################################
+	# Initialize LCD here
+	lcdCols = 16
+	lcdRows = 2
+	i2c = board.I2C()
+	lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcdCols, lcdRows)
+	lcd.clear()
+	lcd.color = [0, 0, 100]
+	lcd.text_direction = lcd.LEFT_TO_RIGHT
+	##########################################################
+
+	while True:
+		if not q.empty():
+			gotSomething = q.get()
+			print(“I got: {}”.format(gotSomething))
+			# Write new data to the LCD here
+
+
+myThread = threading.Thread(target = myFunction, args = ())
+myThread.start()
+
+
+while True:
+	# Do some things…
+	putSomething = random()
+
+	# Send it to the thread
+	if True: # Put your own conditional here (i.e. ArUco marker moved)
+		q.put(putSomething)
+
+	# Carry on…
+	sleep(1) # I wouldn’t keep this here outside of this example
+
