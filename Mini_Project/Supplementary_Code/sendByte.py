302f389c9e19f1dd31c0b9cbc348ba392be0412c
sophiamimlitz <sophiamimlitz@mines.edu>
2025-02-19 14:29:32 -0700

    Update sendByte.py

--- Mini_Project/Supplementary_Code/sendByte.py
+++ Mini_Project/Supplementary_Code/sendByte.py
@@ -1,6 +1,10 @@
+# Sophia Mimlitz
+# Basic Python code required to send a byte to the Arduino
+
 from smbus2 import SMBus
 from time import sleep
 
+# Initialize
 ARD_ADDR = 8
 i2c = SMBus(1)
 offset = 1
@@ -8,4 +12,5 @@ offset = 1
 while(True):
 	# change value of command to be 0, 1, 2, 3 (represents 00, 01, 10, 11)
 	command = 0
+	# Line that writes a byte (command) from  Pi to Arduino
 	i2c.write_byte_data(ARD_ADDR, offset, command)
