075f30d8875657a1c4f60c920bc37ed3571a84b2
YOONSEOKLEE-a <t01053463345@gmail.com>
2025-03-07 14:14:30 -0700

    Add files via upload

---
+++ Demo1/Ardiuno/Matlab/Angular/runForwardResponse.m
@@ -0,0 +1,40 @@
+%% Run Motor Sim Left by Ian Keeffe and Yoon Seok Lee
+
+% Define motor parameters
+K=0.65; % DC gain [rad/Vs]
+sigma=11; % time constant reciprocal [1/s]
+% read motor data
+load('stepData.mat')
+%% Run a Simulation
+
+%
+% open the block diagram so it appears in the documentation when published.
+% Make sure the block diagram is closed before running the publish function
+%
+open_system('forwardResponse')
+%
+% run the simulation
+%
+out=sim('forwardResponse.slx');
+%% A Plot of the results
+%
+figure()
+sgtitle('Response')
+subplot(2,1,1)
+plot(out.voltage,'--','linewidth',2)
+hold on
+plot(data(:,1),(data(:,2)+data(:,3)),'linewidth',2)
+legend('Simulated','Experimental','location','southeast')
+hold off
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+title('Time Series Plot: Voltage')
+subplot(2,1,2)
+plot(out.velocity,'--','linewidth',2)
+hold on
+plot(data(:,1),.075*(data(:,5)-data(:,4))/.362,'linewidth',2)
+hold off
+legend('Simulated','Experimental','location','southeast')
+xlabel('Time (s)')
+ylabel('Velocity (m/s)')
+title('Time Series Plot: Velocity')
